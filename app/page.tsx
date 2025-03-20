8c33607c57fd8a087b73643316812fe8a4739ca2
Daniel Martins <martinsda@gmail.com>
2025-03-20 11:05:32 -0300

    add test fonts main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Typography from "./components/ui/Typography";
 
 export default function Home() {
   return (
-    <Centered>
-      <Typography className="text-4xl">
-        Cinesercla | Painel Administrativo
+    <Centered direction="col" items="start">
+      <Typography className="text-4xl">Neue Montreal</Typography>
+      <Typography className="text-4xl" font="proxima-nova">
+        Proxima Nova
       </Typography>
     </Centered>
   );
