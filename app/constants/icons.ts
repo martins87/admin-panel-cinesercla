03f9f18e12e166be4eef1e5c841e37ef03cb4c07
Daniel Martins <martinsda@gmail.com>
2025-03-20 11:16:11 -0300

    add icons, icons export file

---
+++ app/constants/icons.ts
@@ -0,0 +1,75 @@
+import arrowDown from "@/app/assets/icons/arrowDown.svg";
+import arrowLeft from "@/app/assets/icons/arrowLeft.svg";
+import arrowRight from "@/app/assets/icons/arrowRight.svg";
+import arrowUp from "@/app/assets/icons/arrowUp.svg";
+import arrowUpRight from "@/app/assets/icons/arrowUpRight.svg";
+import bell from "@/app/assets/icons/bell.svg";
+import cadastro from "@/app/assets/icons/cadastro.svg";
+import calendar from "@/app/assets/icons/calendar.svg";
+import chevronDown from "@/app/assets/icons/chevronDown.svg";
+import chevronLeft from "@/app/assets/icons/chevronLeft.svg";
+import chevronRight from "@/app/assets/icons/chevronRight.svg";
+import chevronUp from "@/app/assets/icons/chevronUp.svg";
+import close from "@/app/assets/icons/close.svg";
+import dashboard from "@/app/assets/icons/dashboard.svg";
+import download from "@/app/assets/icons/download.svg";
+import edit from "@/app/assets/icons/edit.svg";
+import eye from "@/app/assets/icons/eye.svg";
+import filter from "@/app/assets/icons/filter.svg";
+import help from "@/app/assets/icons/help.svg";
+import home from "@/app/assets/icons/home.svg";
+import logOut from "@/app/assets/icons/logOut.svg";
+import logo from "@/app/assets/icons/logo.svg";
+import mail from "@/app/assets/icons/mail.svg";
+import password from "@/app/assets/icons/password.svg";
+import pieChart from "@/app/assets/icons/pieChart.svg";
+import plus from "@/app/assets/icons/plus.svg";
+import search from "@/app/assets/icons/search.svg";
+import settings from "@/app/assets/icons/settings.svg";
+import signUp from "@/app/assets/icons/signUp.svg";
+import sort from "@/app/assets/icons/sort.svg";
+import spinner from "@/app/assets/icons/spinner.svg";
+import text from "@/app/assets/icons/text.svg";
+import trash from "@/app/assets/icons/trash.svg";
+import upload from "@/app/assets/icons/upload.svg";
+import user from "@/app/assets/icons/user.svg";
+import users from "@/app/assets/icons/users.svg";
+
+export {
+  arrowDown,
+  arrowLeft,
+  arrowRight,
+  arrowUp,
+  arrowUpRight,
+  bell,
+  cadastro,
+  calendar,
+  chevronDown,
+  chevronLeft,
+  chevronRight,
+  chevronUp,
+  close,
+  dashboard,
+  download,
+  edit,
+  eye,
+  filter,
+  help,
+  home,
+  logOut,
+  logo,
+  mail,
+  password,
+  pieChart,
+  plus,
+  search,
+  settings,
+  signUp,
+  sort,
+  spinner,
+  text,
+  trash,
+  upload,
+  user,
+  users,
+};
