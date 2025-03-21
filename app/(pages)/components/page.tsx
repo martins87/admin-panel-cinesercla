e0a1fbe5a010a0a15ae462866e6353a0debb911a
Daniel Martins <martinsda@gmail.com>
2025-03-21 17:05:48 -0300

    add components page

---
+++ app/(pages)/components/page.tsx
@@ -0,0 +1,20 @@
+import Page from "@/app/components/ui/Page";
+import Centered from "@/app/components/ui/Centered";
+import Typography from "@/app/components/ui/Typography";
+import Link from "next/link";
+
+const ComponentsPage = () => {
+  return (
+    <Page title="Components">
+      <Centered className="gap-y-2" direction="col" items="start">
+        <Link href="/components/button">
+          <Typography className="text-xl text-[#0057FC] hover:underline">
+            Button
+          </Typography>
+        </Link>
+      </Centered>
+    </Page>
+  );
+};
+
+export default ComponentsPage;
