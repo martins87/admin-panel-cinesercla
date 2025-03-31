3e483f1085e1d759c95a771c3b09261011402c7f
Daniel Martins <martinsda@gmail.com>
2025-03-31 18:00:24 -0300

    edit Switch component state

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -5,27 +5,38 @@ import * as SwitchPrimitive from "@radix-ui/react-switch";
 
 import { cn } from "@/lib/utils";
 
-function Switch({
+type SwitchProps = {
+  className?: string;
+  value: boolean;
+  setValue: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Switch: React.FC<SwitchProps> = ({
   className,
-  ...props
-}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+  value: checked,
+  setValue,
+}) => {
   return (
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer data-[state=checked]:bg-[#0057FC] data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-5 w-9 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 hover:cursor-pointer",
+        "peer focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-5 w-9 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 hover:cursor-pointer",
+        checked ? "bg-[#0057FC] " : "bg-input dark:bg-input/80",
         className
       )}
-      {...props}
+      onClick={() => setValue(!checked)}
     >
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%+1px)] data-[state=unchecked]:translate-x-[1.5px]"
+          "bg-background pointer-events-none block size-4 rounded-full ring-0 transition-transform",
+          checked
+            ? "dark:bg-primary-foreground translate-x-[calc(100%+1px)]"
+            : "dark:bg-foreground translate-x-[1.5px]"
         )}
       />
     </SwitchPrimitive.Root>
   );
-}
+};
 
-export { Switch };
+export default Switch;
