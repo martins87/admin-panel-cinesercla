b8026d8187f911126ec59613a43a8ef780a461ca
Daniel Martins <martinsda@gmail.com>
2025-04-01 15:51:37 -0300

    add AlertModal to edit faq page

--- app/(pages)/cadastro/perguntas-frequentes/editar/[id]/page.tsx
+++ app/(pages)/cadastro/perguntas-frequentes/editar/[id]/page.tsx
@@ -13,6 +13,7 @@ import Input from "@/app/components/ui/Input";
 import Button from "@/app/components/ui/Button";
 import ComboBox from "@/app/components/ui/ComboBox";
 import FAQInput from "@/app/components/FAQ/FAQInput";
+import AlertModal from "@/app/components/AlertModal";
 import { categorias, opcoes } from "@/app/constants/faq";
 import { updateFaq } from "@/lib/db/faq";
 
@@ -36,6 +37,9 @@ const EditarPerguntasFrequentePage = ({
   const [cadastro, setCadastro] = useState<string>("");
   const [cliques, setCliques] = useState<string>("");
   const [ativa, setAtiva] = useState<boolean>(false);
+  const [salvarModalOpen, setSalvarModalOpen] = useState<boolean>(false);
+  const [salvarESairModalOpen, setSalvarESairModalOpen] =
+    useState<boolean>(false);
 
   useEffect(() => {
     // TODO add loading...
@@ -54,7 +58,7 @@ const EditarPerguntasFrequentePage = ({
     }
   }, [id, getFaqById, router]);
 
-  const handleSalvar = async () => {
+  const handleSalvar = async (sair?: boolean) => {
     const editedFaq: Faq = {
       _id: id,
       pergunta,
@@ -77,7 +81,9 @@ const EditarPerguntasFrequentePage = ({
         // Updates Zustand store
         updateFaqList(updatedFaq);
 
-        router.push("/cadastro/perguntas-frequentes"); // Redirect on success
+        if (sair) {
+          router.push("/cadastro/perguntas-frequentes");
+        }
       } else {
         console.error("Failed to update FAQ.");
       }
@@ -89,64 +95,92 @@ const EditarPerguntasFrequentePage = ({
   const handleCancelar = () => router.back();
 
   return (
-    <Page
-      title="Editar Pergunta Frequente"
-      subtitle="Corrija, atualize ou reorganize perguntas frequentes"
-    >
-      <Centered className="gap-y-4" direction="col">
-        <Centered className="gap-x-2" items="center" justify="start">
-          <Typography>Ativa</Typography>
-          <Switch value={ativa} setValue={setAtiva} />
-        </Centered>
-        <Centered className="grid grid-cols-2 gap-x-4 gap-y-4">
-          <FAQInput label="Selecione uma Categoria" obrigatoria>
-            <ComboBox
-              label="Selecione"
-              list={categorias}
-              value={categoria}
-              setValue={setCategoria}
-            />
-          </FAQInput>
-          <FAQInput label="Ordem dentro do Grupo" obrigatoria>
-            <Input placeholder={ordem} value={ordem} setValue={setOrdem} />
-          </FAQInput>
-          <FAQInput label="Agrupar nas Principais Dúvidas?">
-            <ComboBox
-              label={principalDuvida ? "Sim" : "Não"}
-              list={opcoes}
-              value={principalDuvida}
-              setValue={setPrincipalDuvida}
-            />
-          </FAQInput>
-          <FAQInput label="Ordem Entre as Principais Dúvidas">
+    <>
+      <Page
+        title="Editar Pergunta Frequente"
+        subtitle="Corrija, atualize ou reorganize perguntas frequentes"
+      >
+        <Centered className="gap-y-4" direction="col">
+          <Centered className="gap-x-2" items="center" justify="start">
+            <Typography>Ativa</Typography>
+            <Switch value={ativa} setValue={setAtiva} />
+          </Centered>
+          <Centered className="grid grid-cols-2 gap-x-4 gap-y-4">
+            <FAQInput label="Selecione uma Categoria" obrigatoria>
+              <ComboBox
+                label="Selecione"
+                list={categorias}
+                value={categoria}
+                setValue={setCategoria}
+              />
+            </FAQInput>
+            <FAQInput label="Ordem dentro do Grupo" obrigatoria>
+              <Input placeholder={ordem} value={ordem} setValue={setOrdem} />
+            </FAQInput>
+            <FAQInput label="Agrupar nas Principais Dúvidas?">
+              <ComboBox
+                label={principalDuvida ? "Sim" : "Não"}
+                list={opcoes}
+                value={principalDuvida}
+                setValue={setPrincipalDuvida}
+              />
+            </FAQInput>
+            <FAQInput label="Ordem Entre as Principais Dúvidas">
+              <Input
+                placeholder={ordemPrincipalDuvida}
+                value={ordemPrincipalDuvida}
+                setValue={setOrdemPrincipalDuvida}
+              />
+            </FAQInput>
+          </Centered>
+          <FAQInput label="Digite uma Pergunta" obrigatoria>
             <Input
-              placeholder={ordemPrincipalDuvida}
-              value={ordemPrincipalDuvida}
-              setValue={setOrdemPrincipalDuvida}
+              placeholder={pergunta}
+              value={pergunta}
+              setValue={setPergunta}
             />
           </FAQInput>
+          <FAQInput label="Digite uma Resposta" obrigatoria>
+            <Input
+              placeholder={resposta}
+              value={resposta}
+              setValue={setResposta}
+            />
+          </FAQInput>
+          <Centered className="gap-x-2" justify="end">
+            <Button
+              label="SALVAR"
+              primary
+              onClick={() => setSalvarModalOpen(true)}
+            />
+            <Button
+              label="SALVAR E SAIR"
+              secondary
+              onClick={() => setSalvarESairModalOpen(true)}
+            />
+            <Button label="CANCELAR" secondary onClick={handleCancelar} />
+          </Centered>
         </Centered>
-        <FAQInput label="Digite uma Pergunta" obrigatoria>
-          <Input
-            placeholder={pergunta}
-            value={pergunta}
-            setValue={setPergunta}
-          />
-        </FAQInput>
-        <FAQInput label="Digite uma Resposta" obrigatoria>
-          <Input
-            placeholder={resposta}
-            value={resposta}
-            setValue={setResposta}
-          />
-        </FAQInput>
-        <Centered className="gap-x-2" justify="end">
-          <Button label="SALVAR" primary onClick={handleSalvar} />
-          <Button label="SALVAR E SAIR" secondary />
-          <Button label="CANCELAR" secondary onClick={handleCancelar} />
-        </Centered>
-      </Centered>
-    </Page>
+      </Page>
+      <AlertModal
+        isOpen={salvarModalOpen}
+        title="Deseja salvar?"
+        message="Ao confirmar, ação não poderá ser desfeita."
+        confirmLabel="SALVAR"
+        onCancel={() => setSalvarModalOpen(false)}
+        onConfirm={() => handleSalvar(false)}
+        hideOnOutsideClick={true}
+      />
+      <AlertModal
+        isOpen={salvarESairModalOpen}
+        title="Deseja salvar e sair?"
+        message="Ao confirmar, ação não poderá ser desfeita."
+        confirmLabel="SALVAR E SAIR"
+        onCancel={() => setSalvarESairModalOpen(false)}
+        onConfirm={() => handleSalvar(true)}
+        hideOnOutsideClick={true}
+      />
+    </>
   );
 };
 
