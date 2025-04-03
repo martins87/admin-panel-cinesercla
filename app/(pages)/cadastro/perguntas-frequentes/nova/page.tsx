8f377ff23ae003d5da17aa5a5dd89b1d91ffe029
Daniel Martins <martinsda@gmail.com>
2025-04-03 19:12:39 -0300

    add nova faq page

---
+++ app/(pages)/cadastro/perguntas-frequentes/nova/page.tsx
@@ -0,0 +1,15 @@
+import Page from "@/app/components/ui/Page";
+import Typography from "@/app/components/ui/Typography";
+
+const NovaPerguntasFrequentePage = () => {
+  return (
+    <Page
+      title="Nova Pergunta Frequente"
+      subtitle="Adicione uma nova pergunta frequente e defina sua categoria"
+    >
+      <Typography>nova</Typography>
+    </Page>
+  );
+};
+
+export default NovaPerguntasFrequentePage;
--- app/(pages)/cadastro/perguntas-frequentes/page.tsx
+++ app/(pages)/cadastro/perguntas-frequentes/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 import { Faq } from "@/app/types/Faq";
 import { useFaqStore } from "@/app/store/faq";
 import Page from "@/app/components/ui/Page";
 import Centered from "@/app/components/ui/Centered";
 import PerguntaFrequente from "@/app/components/FAQ/PerguntaFrequente";
+import Button from "@/app/components/ui/Button";
 
 const FaqPage = () => {
+  const router = useRouter();
   const { faqList, fetchFaqList } = useFaqStore();
 
   useEffect(() => {
@@ -23,6 +26,13 @@ const FaqPage = () => {
       title="Lista de Perguntas Frequentes"
       subtitle="Visualize e gerencie todas as perguntas frequentes"
     >
+      <Centered justify="start">
+        <Button
+          label="NOVA PERGUNTA FREQUENTE"
+          primary
+          onClick={() => router.push("/cadastro/perguntas-frequentes/nova")}
+        />
+      </Centered>
       <Centered direction="col" className="gap-y-4">
         {faqList.map((faq: Faq, index: number) => (
           <PerguntaFrequente key={index} faq={faq} />
