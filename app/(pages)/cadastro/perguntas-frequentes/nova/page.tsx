a45f7d65f0c43be5f2c14804e8a2412d34084962
Daniel Martins <martinsda@gmail.com>
2025-04-03 19:34:52 -0300

    add nova faq page fields

--- app/(pages)/cadastro/perguntas-frequentes/nova/page.tsx
+++ app/(pages)/cadastro/perguntas-frequentes/nova/page.tsx
@@ -1,14 +1,131 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import Page from "@/app/components/ui/Page";
+import Centered from "@/app/components/ui/Centered";
 import Typography from "@/app/components/ui/Typography";
+import Button from "@/app/components/ui/Button";
+import AlertModal from "@/app/components/AlertModal";
+import Switch from "@/components/ui/switch";
+import FAQInput from "@/app/components/FAQ/FAQInput";
+import ComboBox from "@/app/components/ui/ComboBox";
+import Input from "@/app/components/ui/Input";
+import { categorias, opcoes } from "@/app/constants/faq";
 
 const NovaPerguntasFrequentePage = () => {
+  const router = useRouter();
+  const [categoria, setCategoria] = useState<string | boolean>("");
+  const [ordem, setOrdem] = useState<string>("");
+  const [principalDuvida, setPrincipalDuvida] = useState<string | boolean>(
+    false
+  );
+  const [ordemPrincipalDuvida, setOrdemPrincipalDuvida] = useState<string>("-");
+  const [pergunta, setPergunta] = useState<string>("");
+  const [resposta, setResposta] = useState<string>("");
+  // const [cadastro, setCadastro] = useState<string>("");
+  // const [cliques, setCliques] = useState<string>("");
+  const [ativa, setAtiva] = useState<boolean>(false);
+  const [salvarModalOpen, setSalvarModalOpen] = useState<boolean>(false);
+  const [salvarESairModalOpen, setSalvarESairModalOpen] =
+    useState<boolean>(false);
+
+  const handleSalvar = (sair?: boolean) => {
+    if (sair) {
+      router.push("/cadastro/perguntas-frequentes");
+    }
+  };
+
+  const handleVoltar = () => router.back();
+
   return (
-    <Page
-      title="Nova Pergunta Frequente"
-      subtitle="Adicione uma nova pergunta frequente e defina sua categoria"
-    >
-      <Typography>nova</Typography>
-    </Page>
+    <>
+      <Page
+        title="Nova Pergunta Frequente"
+        subtitle="Adicione uma pergunta frequente e defina sua categoria"
+      >
+        <Centered className="gap-y-4" direction="col">
+          <Centered className="gap-x-2" items="center" justify="start">
+            <Typography weight="500">Ativa</Typography>
+            <Switch value={ativa} setValue={setAtiva} />
+          </Centered>
+          <Centered className="grid grid-cols-2 gap-x-4 gap-y-4">
+            <FAQInput label="Selecione uma Categoria" obrigatoria>
+              <ComboBox
+                label="Selecione"
+                list={categorias}
+                value={categoria}
+                setValue={setCategoria}
+              />
+            </FAQInput>
+            <FAQInput label="Ordem dentro do Grupo" obrigatoria>
+              <Input placeholder={ordem} value={ordem} setValue={setOrdem} />
+            </FAQInput>
+            <FAQInput label="Agrupar nas Principais Dúvidas?">
+              <ComboBox
+                label={principalDuvida ? "Sim" : "Não"}
+                list={opcoes}
+                value={principalDuvida}
+                setValue={setPrincipalDuvida}
+              />
+            </FAQInput>
+            <FAQInput label="Ordem Entre as Principais Dúvidas">
+              <Input
+                placeholder={ordemPrincipalDuvida}
+                value={ordemPrincipalDuvida}
+                setValue={setOrdemPrincipalDuvida}
+              />
+            </FAQInput>
+          </Centered>
+          <FAQInput label="Digite uma Pergunta" obrigatoria>
+            <Input
+              placeholder={pergunta}
+              value={pergunta}
+              setValue={setPergunta}
+            />
+          </FAQInput>
+          <FAQInput label="Digite uma Resposta" obrigatoria>
+            <Input
+              placeholder={resposta}
+              value={resposta}
+              setValue={setResposta}
+            />
+          </FAQInput>
+          <Centered className="gap-x-2" justify="end">
+            <Button
+              label="SALVAR"
+              primary
+              onClick={() => setSalvarModalOpen(true)}
+            />
+            <Button
+              label="SALVAR E SAIR"
+              secondary
+              onClick={() => setSalvarESairModalOpen(true)}
+            />
+            <Button label="VOLTAR" secondary onClick={handleVoltar} />
+          </Centered>
+        </Centered>
+      </Page>
+      <AlertModal
+        isOpen={salvarModalOpen}
+        title="Deseja salvar?"
+        message="Ao confirmar, ação não poderá ser desfeita."
+        confirmLabel="SALVAR"
+        onCancel={() => setSalvarModalOpen(false)}
+        onConfirm={() => handleSalvar(false)}
+        hideOnOutsideClick={true}
+      />
+      <AlertModal
+        isOpen={salvarESairModalOpen}
+        title="Deseja salvar e sair?"
+        message="Ao confirmar, ação não poderá ser desfeita."
+        confirmLabel="SALVAR E SAIR"
+        onCancel={() => setSalvarESairModalOpen(false)}
+        onConfirm={() => handleSalvar(true)}
+        hideOnOutsideClick={true}
+      />
+    </>
   );
 };
 
