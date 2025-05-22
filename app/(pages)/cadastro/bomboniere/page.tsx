92bed9c492dd5f296e987e7584aa1a1a771d43cf
Daniel Martins <martinsda@gmail.com>
2025-05-21 21:11:51 -0300

    add loading feedback to bomboniere page

--- app/(pages)/cadastro/bomboniere/page.tsx
+++ app/(pages)/cadastro/bomboniere/page.tsx
@@ -5,14 +5,13 @@ import { useRouter } from "next/navigation";
 import { useBomboniere } from "@/app/hooks/useBomboniere";
 import Button from "@/app/components/ui/Button";
 import Page from "@/app/components/ui/Page";
+import Typography from "@/app/components/ui/Typography";
+import Loading from "@/app/components/Loading";
 import BomboniereRow from "@/app/components/Bomboniere/BomboniereRow";
 
 const BombonierePage = () => {
   const router = useRouter();
-  const {
-    data: productList,
-    // isFetching
-  } = useBomboniere();
+  const { data: productList, isFetching, isSuccess, isError } = useBomboniere();
   console.log("product list", productList);
 
   const handleNovoProduto = () =>
@@ -26,9 +25,15 @@ const BombonierePage = () => {
         <Button label="NOVO PRODUTO" primary onClick={handleNovoProduto} />
       }
     >
-      {productList?.map((product) => (
-        <BomboniereRow key={product._id} product={product} />
-      ))}
+      {isFetching ? (
+        <Loading />
+      ) : isSuccess ? (
+        productList?.map((product) => (
+          <BomboniereRow key={product._id} product={product} />
+        ))
+      ) : isError ? (
+        <Typography>Erro buscando lista de produtos</Typography>
+      ) : null}
     </Page>
   );
 };
