cf9613c1b3ce7257c66438404de47e402d7c927b
Daniel Martins <martinsda@gmail.com>
2025-05-23 15:57:36 -0300

    edit ComboBox styling

--- app/components/ui/ComboBox.tsx
+++ app/components/ui/ComboBox.tsx
@@ -52,14 +52,14 @@ const ComboBox: FC<ComboBoxProps> = ({
   return (
     <Centered
       className={twMerge(
-        "relative px-2 py-2 gap-x-0 gap-y-1 rounded-lg bg-white border",
+        "relative px-2 py-2 gap-x-0 gap-y-1 rounded-lg bg-white border hover:cursor-pointer",
         className
       )}
       direction="col"
       onClick={() => setShowItems(!showItems)}
       ref={comboBoxRef}
     >
-      <Centered className="px-2 py-0 hover:cursor-pointer" justify="between">
+      <Centered className="px-2 py-0" justify="between">
         <Typography>
           {showItems ? label : list.find((item) => item.value === value)?.label}
         </Typography>
@@ -70,7 +70,7 @@ const ComboBox: FC<ComboBoxProps> = ({
           {list.map((item, index) => (
             <li
               key={index}
-              className="w-full px-4 py-2 hover:bg-[#A3A3A3]/10 hover:cursor-pointer"
+              className="w-full px-4 py-2 hover:bg-[#A3A3A3]/10"
               onClick={() => setValue(item.value)}
             >
               <Typography>{item.label}</Typography>
